d675b864d5464e643f422599dce5808591dfcc68
Ian Stewart <istewart@nvidia.com>
2022-04-26 15:09:39 -0600

    Add ajantv2 CMake package config
    This adds CMake rules to output the ajantv2
    config such that the ajantv2 library can be
    built and linked against using the following:
    
      find_package(ajantv2 16.2 REQUIRED)
      target_link_libraries(<target> AJA::ajantv2)

--- cmake/AJACommonDefines.cmake
+++ cmake/AJACommonDefines.cmake
@@ -1,16 +1,16 @@
 # Common preprocessor defines
 if(CMAKE_BUILD_TYPE MATCHES Debug)
     aja_message(STATUS "Build Type: Debug")
-    add_definitions(-DAJA_DEBUG -D_DEBUG)
+    list(APPEND AJANTV2_COMPILE_DEFINITIONS -DAJA_DEBUG -D_DEBUG)
 elseif(CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)
     aja_message(STATUS "Build Type: Release with Debug Symbols")
 else()
     aja_message(STATUS "Build Type: Release")
-    add_definitions(-DNDEBUG)
+    list(APPEND AJANTV2_COMPILE_DEFINITIONS -DNDEBUG)
 endif()
 # Platform-specific preprocessor defines
 if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
-    add_definitions(
+    list(APPEND AJANTV2_COMPILE_DEFINITIONS
         -DAJA_WINDOWS
         -DMSWindows
         -D_WINDOWS
@@ -21,12 +21,14 @@ if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
         -D_CRT_SECURE_NO_WARNINGS
         -D_SCL_SECURE_NO_WARNINGS)
 elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    add_definitions(
+    list(APPEND AJANTV2_COMPILE_DEFINITIONS
         -DAJALinux
         -DAJA_LINUX)
 elseif (CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-    add_definitions(
+    list(APPEND AJANTV2_COMPILE_DEFINITIONS
         -DAJAMac
         -DAJA_MAC
         -D__STDC_CONSTANT_MACROS)
 endif()
+
+add_definitions(${AJANTV2_COMPILE_DEFINITIONS})
